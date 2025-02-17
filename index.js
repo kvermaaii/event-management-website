14e0a2de5b21bd90e037537f0e9d20cff00e9f65
Kshitij Verma <kvermaaii@gmail.com>
2025-02-17 22:19:18 +0530

    add login, sign-up page and base.ejs

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');  // Set up EJS for templating
+app.set('views', './views');
+
 app.use(express.static("Public"))
 
 app.get('/', (req, res) => {
   res.render("home.ejs");
 })
 
+app.get('/login', (req, res) => {
+  res.render('base', { title: 'Login', content: 'login', showLogin: false, 
+    showSignup: true  });
+})
+
+app.get('/sign-up', (req, res) => {
+  res.render('base', { title: 'Sign-up', content: 'sign-up', showLogin: true, 
+    showSignup: false });
+})
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
